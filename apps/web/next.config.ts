c9f8ec1c9771efc275c2ef0c147b78c3b1d1a93c
iamVihanga <vihangarashansilva@gmail.com>
2025-06-05 14:54:46 +0530

    Add: NextJS Proxy Route to Backend Server

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@repo/ui"]
+  transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*"
+      }
+    ];
+  }
 };
 
 export default nextConfig;
