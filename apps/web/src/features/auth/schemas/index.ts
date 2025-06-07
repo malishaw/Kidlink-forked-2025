389feb4bb58b33c930950e8abacbf49e44414124
iamVihanga <vihangarashansilva@gmail.com>
2025-06-07 14:56:59 +0530

    Migrate: React-Hook-Form to Tanstack/Forms

---
+++ apps/web/src/features/auth/schemas/index.ts
@@ -0,0 +1,4 @@
+export * from "./forgot-password-schema";
+export * from "./reset-password-schema";
+export * from "./signin-schema";
+export * from "./signup-schema";
--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,12 +1,16 @@
-import { adminClient, apiKeyClient } from "better-auth/client/plugins";
+import {
+  adminClient,
+  apiKeyClient,
+  organizationClient
+} from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { toast } from "sonner";
 
 import { env } from "@/lib/env";
 
 export const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_APP_URL,
-  plugins: [adminClient(), apiKeyClient()],
+  baseURL: env.NEXT_PUBLIC_BACKEND_URL,
+  plugins: [adminClient(), apiKeyClient(), organizationClient()],
   fetchOptions: {
     onError: (ctx) => {
       toast.error(ctx.error.message);
